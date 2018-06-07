4c6706784e9789cc6fda31704a0f6a73771ba537
sonichy <sonichy@163.com>
2018-06-07 15:35:31 +0800

    缩小跳到今天按钮宽度

--- calendarwidget.cpp
+++ calendarwidget.cpp
@@ -8,7 +8,8 @@ CalendarWidget::CalendarWidget(QWidget *parent) : QWidget(parent)
     QVBoxLayout *vbox = new QVBoxLayout;
     QHBoxLayout *hbox = new QHBoxLayout;
     hbox->addStretch();
-    QPushButton *pushButton_today = new QPushButton("今天");
+    QPushButton *pushButton_today = new QPushButton("今");
+    pushButton_today->setFixedWidth(30);
     connect(pushButton_today,SIGNAL(pressed()),this,SLOT(jumpToToday()));
     hbox->addWidget(pushButton_today);
     vbox->addLayout(hbox);
